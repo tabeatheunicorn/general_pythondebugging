7d0e387c8dca62328641d7c58d4b02a10eb6e28b
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2021-08-24 11:43:21 +0200

    Saving and plotting decorator works, too

--- Decorators/simpleplot.py
+++ Decorators/simpleplot.py
@@ -4,65 +4,60 @@ import pandas as pd
 import numpy as np
 
 
-def save_fig(func, **param):
-    """ Plots and/or saves a figure depending on what parameters are passed to save_fig"""
-    def inner(*args, **kwargs):
-        artist = func(*args, **kwargs)
-        if 'filename' in param.keys():
-            print('filename')
-            plt.savefig(param['filename'])
-        if 'show' in param.keys() and param["show"]:
-            print('show')
-            plt.show()
-        else:
-            return artist
-    return inner
+def save_figure_factory(**param):
+    """ Plots and/or saves a figure depending on what parameters are passed to save_fig.
+        Expects the function to draw some kind of plot.
+    
+        Possible params:
+        filename: saves the figure to given str if present.
+        show:     shows the resulting figure if present.
+        else:     if none of the above is given, this just returns the function.
+        """
+    def func_decorator(func):
+        @functools.wraps(func)
+        def inner(*args, **kwargs):
+            artist = func(*args, **kwargs)
+            if 'filename' in param.keys():
+                print('filename')
+                plt.savefig(param['filename'])
+            if 'show' in param.keys() and param["show"]:
+                print('show')
+                plt.show()
+            else:
+                return artist
+        return inner
+    return func_decorator
 
 
-def save_show_no(plotfunc):
-    # A function decorator that adds the option to save or show a plot
-    # depending on whether a filename option is set.
-
-    def decorate(*args, **kwargs):
-        ax = plotfunc(*args)
-
-        if 'filename' in kwargs.keys():
-            plt.savefig(kwargs['filename'])
-
-        elif 'show' in kwargs.keys():
-            plt.show()
-
-        else:
-            return ax
-
-    return decorate
-
-def overplot_functionfactory(x,y):
+def overplot_functionfactory(x, y):
+    """Add a plot with x,y datapairs to the plot that the decorated function creates.
+        Function must not create a new figure in order for this to work!"""
     def overplot(func):
-        """Acutally plot before and after function"""
         @functools.wraps(func)
         def testplot_wrapper(*args, **kwargs):
+            """Acutally plot before plotting function is called"""
             plt.plot(x, y)
             return func(*args, **kwargs)
         return testplot_wrapper
     return overplot
 
-@overplot_functionfactory(x=np.linspace(0,5), 
-                          y= np.linspace(0,20))
-def testplot():
-    x, y = np.linspace(0,5), np.linspace(0,5)
-    plt.plot(x,y) 
-
 
 if __name__ == '__main__':
-    # df = pd.DataFrame(data={
-    #     'foo': list(range(5)),
-    #     'bar': list(range(5, 10, 1))
-    # })
+    df = pd.DataFrame(data={
+        'foo': list(range(5)),
+        'bar': list(range(5, 10, 1))
+    })
 
-    # @save_fig(**{'filename': 'foo.png', 'show': True})
-    # def plot_this():
-    #     return plt.scatter(df['foo'], df['bar'])
-
-    testplot()
-    plt.show() 
+    @save_figure_factory(**{'filename': 'foo.png', 'show': True})
+    def plot_this():
+        return plt.scatter(df['foo'], df['bar'])
+    
+    plot_this()
+    # @overplot_functionfactory(x=np.linspace(0, 5),
+    #                           y=np.linspace(0, 20))
+    # def testplot():
+    #     x, y = np.linspace(0, 5), np.linspace(0, 5)
+    #     plt.plot(x, y)
+        
+    # testplot()
+    # plt.show()
