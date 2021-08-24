03b99b1a12ed763dc2d6fbb7d698cf35a48fecb5
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2021-08-24 12:12:12 +0200

    further packaing details

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Debugging Functions and Decorators useful within any project'
+LONG_DESCRIPTION = 'Libary of often useful functions that are collected to be maintained here.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="generichelpermodule", 
+        version=VERSION,
+        author="Tabea Röthemeyer",
+        author_email="tabea.roethemeyer@gruppe.ai",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], 
+        url="",
+        
+        keywords=['python', 'debugging'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
