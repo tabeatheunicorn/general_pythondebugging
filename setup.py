a0bcb9e0ff2741d24c52a02def1787200f327c21
Tabea <Tabea.Roethemeyer@rub.de>
2021-08-26 14:28:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
         install_requires=[
-               'time',
                'logging',
                'matplotlib',
                'seaborn',
