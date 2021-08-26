8b4a16e4730e397543ca26922c688d13bda3c7be
Tabea <Tabea.Roethemeyer@rub.de>
2021-08-26 14:19:07 +0200

    Update setup.py
    functools already satisfied, not installable

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=['functools',
+        install_requires=[
                'time',
                'logging',
                'matplotlib',
